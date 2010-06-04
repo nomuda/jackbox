abd4a7bea8c4b76c74959db7072fdd4b26eaf79d
Ole Christian Rynning <oc@rynning.no>
2010-06-04 14:31:58 +0200

    Added annotation for Recording

--- src/test/java/no/muda/jackbox/example/ExampleRecordedObject.java
+++ src/test/java/no/muda/jackbox/example/ExampleRecordedObject.java
@@ -1,9 +1,12 @@
 package no.muda.jackbox.example;
 
+import no.muda.jackbox.annotations.Recording;
+
 public class ExampleRecordedObject {
 
     private ExampleDependency exampleDependency;
 
+    @Recording
     public int exampleMethod(Object parameter, Object parameter2) {
         return 0;
     }
