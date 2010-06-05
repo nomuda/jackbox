5b4312e252ffabbdca5b67ddfcbc64281dd1cff3
Ole Christian Rynning <oc@rynning.no>
2010-06-05 16:02:03 +0200

    I suggest these simple conventions: * 4 spaces for indentation (soft tabs) * LF (Unix) line endings * Max line length 130 * Do not anonymize imports (java.package.*)
    (We should also agree on some import order)

--- src/test/java/no/muda/jackbox/persistance/json/JSONPersistanceTest.java
+++ src/test/java/no/muda/jackbox/persistance/json/JSONPersistanceTest.java
@@ -1,38 +1,40 @@
 package no.muda.jackbox.persistance.json;
 
+import static org.fest.assertions.Assertions.assertThat;
+
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
 import no.muda.jackbox.DependencyRecording;
 import no.muda.jackbox.MethodRecording;
-import no.muda.jackbox.example.*;
-import org.junit.Test;
+import no.muda.jackbox.example.ExampleDependency;
+import no.muda.jackbox.example.ExampleRecordedObject;
 
-import static org.fest.assertions.Assertions.assertThat;
+import org.junit.Test;
 
 public class JSONPersistanceTest {
     final static String testArgumentValue = "abcd";
     final static String testArgumentReturnValue = "efgh";
 
     @Test
-	public void testPersistToString() throws Exception {
-		DependencyRecording recording = getTestRecording();
+    public void testPersistToString() throws Exception {
+        DependencyRecording recording = getTestRecording();
 
-		Persister persister = new JSONPersister();
-		String result = persister.persistToString(recording);
+        Persister persister = new JSONPersister();
+        String result = persister.persistToString(recording);
 
         assertThat(result).contains("invokedMethodOnDependency")
-            .contains(testArgumentValue)
-            .contains(testArgumentReturnValue);
-	}
+                .contains(testArgumentValue)
+                .contains(testArgumentReturnValue);
+    }
 
-	private DependencyRecording getTestRecording() throws NoSuchMethodException {
-		DependencyRecording recording = new DependencyRecording(ExampleRecordedObject.class);
-		Method testMethod = ExampleDependency.class.getMethod("invokedMethodOnDependency", String.class);
-		MethodRecording testMethodrecording = new MethodRecording(ExampleDependency.class, testMethod, Arrays.asList(testArgumentValue) );
-		testMethodrecording.setReturnValue(testArgumentReturnValue);
-		recording.addMethodRecording(testMethodrecording);
+    private DependencyRecording getTestRecording() throws NoSuchMethodException {
+        DependencyRecording recording = new DependencyRecording(ExampleRecordedObject.class);
+        Method testMethod = ExampleDependency.class.getMethod("invokedMethodOnDependency", String.class);
+        MethodRecording testMethodrecording = new MethodRecording(ExampleDependency.class, testMethod, Arrays.asList(testArgumentValue));
+        testMethodrecording.setReturnValue(testArgumentReturnValue);
+        recording.addMethodRecording(testMethodrecording);
 
-		return recording;
-	}
+        return recording;
+    }
 }
