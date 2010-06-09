d2ab468f5cf56125e753c19e44edb479c16c3120
Ole Christian Rynning <oc@rynning.no>
2010-06-09 15:35:42 +0200

    Added ClassAnnotated demo app

---
+++ src/test/java/no/muda/jackbox/example/classannotation/ClassAnnotationDemoService.java
@@ -0,0 +1,22 @@
+package no.muda.jackbox.example.classannotation;
+
+import no.muda.jackbox.annotations.Recording;
+import no.muda.jackbox.example.Entity;
+
+import java.util.List;
+
+@Recording
+public class ClassAnnotationDemoService {
+
+    private ClassAnnotationDemoDao dao = new ClassAnnotationDemoDao();
+
+    public int doSomething() {
+        System.out.println("In " + getClass());
+        List<Entity> entities = dao.findAll();
+        for (Entity entity : entities) {
+            System.out.println(entity);
+        }
+        return entities.size();
+    }
+
+}
