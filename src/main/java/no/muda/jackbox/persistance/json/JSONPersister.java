77f0f2642542f0dc79fc2a392966aa7257ca7f86
Marius B. Kotsbak <marius@kotsbak.com>
2010-06-12 00:36:58 +0200

    Fixed correct formating and indention.

--- src/main/java/no/muda/jackbox/persistance/json/JSONPersister.java
+++ src/main/java/no/muda/jackbox/persistance/json/JSONPersister.java
@@ -20,74 +20,82 @@ import com.google.gson.JsonSerializer;
 
 public class JSONPersister implements Persister {
     public void persistToWriter(MethodRecording recording, Appendable output) {
-        Gson gson = new GsonBuilder()
-            .setPrettyPrinting()
-            .registerTypeAdapter(MethodRecording.class, new MethodRecordingTypeAdaptor())
-            .registerTypeAdapter(Method.class, new MethodTypeAdapter())
-            .create();
+        Gson gson = new GsonBuilder().setPrettyPrinting().registerTypeAdapter(
+                MethodRecording.class, new MethodRecordingTypeAdaptor())
+                .registerTypeAdapter(Method.class, new MethodTypeAdapter())
+                .create();
         gson.toJson(recording, output);
     }
 
     public MethodRecording readFromReader(Reader input) {
-        Gson gson = new GsonBuilder()
-            .registerTypeAdapter(MethodRecording.class, new MethodRecordingTypeAdaptor())
-            .registerTypeAdapter(Method.class, new MethodTypeAdapter())
-            .create();
+        Gson gson = new GsonBuilder().registerTypeAdapter(
+                MethodRecording.class, new MethodRecordingTypeAdaptor())
+                .registerTypeAdapter(Method.class, new MethodTypeAdapter())
+                .create();
 
         return gson.fromJson(input, MethodRecording.class);
     }
 }
 
-class MethodRecordingTypeAdaptor implements JsonSerializer<MethodRecording>, JsonDeserializer<MethodRecording> {
+class MethodRecordingTypeAdaptor implements JsonSerializer<MethodRecording>,
+        JsonDeserializer<MethodRecording> {
 
-    public JsonElement serialize(MethodRecording src, Type typeOfSrc, JsonSerializationContext context) {
-		  JsonObject obj = new JsonObject();
-		  obj.addProperty("classname", src.getTargetClass().getCanonicalName());
-		  obj.add("method", context.serialize(src.getMethod()));
-		  obj.add("arguments", context.serialize(Arrays.asList(src.getArguments())));
-		  if (src.getMethod().getReturnType() != Void.TYPE) {
-		      obj.add("returnvalue", context.serialize(src.getRecordedResult(),
-		          src.getMethod().getGenericReturnType()));
-		  }
-		  obj.add("dependencies", context.serialize(src.getDependencyMethodRecordings()));
+    public JsonElement serialize(MethodRecording src, Type typeOfSrc,
+            JsonSerializationContext context) {
+        JsonObject obj = new JsonObject();
+        obj.addProperty("classname", src.getTargetClass().getCanonicalName());
+        obj.add("method", context.serialize(src.getMethod()));
+        obj.add("arguments", context.serialize(Arrays
+                .asList(src.getArguments())));
+        if (src.getMethod().getReturnType() != Void.TYPE) {
+            obj.add("returnvalue", context.serialize(src.getRecordedResult(),
+                    src.getMethod().getGenericReturnType()));
+        }
+        obj.add("dependencies", context.serialize(src
+                .getDependencyMethodRecordings()));
 
-		  return obj;
-	  }
+        return obj;
+    }
 
-	  public MethodRecording deserialize(JsonElement element, Type typeOfSrc,
-			JsonDeserializationContext context) throws JsonParseException {
-		JsonObject obj = element.getAsJsonObject();
-		Class<?> clazz = getTargetClass(obj);
+    public MethodRecording deserialize(JsonElement element, Type typeOfSrc,
+            JsonDeserializationContext context) throws JsonParseException {
+        JsonObject obj = element.getAsJsonObject();
+        Class<?> clazz = getTargetClass(obj);
 
-		Method method = context.deserialize(obj.get("method"), Method.class);
+        Method method = context.deserialize(obj.get("method"), Method.class);
 
-		Object returnValue;
-		if (method.getReturnType() != Void.TYPE) {
-		    returnValue = context.deserialize(obj.get("returnvalue"), method.getGenericReturnType());
-		}
-		else returnValue = null;
+        Object returnValue;
+        if (method.getReturnType() != Void.TYPE) {
+            returnValue = context.deserialize(obj.get("returnvalue"), method
+                    .getGenericReturnType());
+        } else
+            returnValue = null;
 
-		JsonArray jsonArguments = (JsonArray) obj.get("arguments");
-		Object[] arguments = new Object[method.getParameterTypes().length];
-		for (int i = 0; i < jsonArguments.size(); i++) {
-		    arguments[i] = context.deserialize(jsonArguments.get(i), method.getGenericParameterTypes()[i]);
+        JsonArray jsonArguments = (JsonArray) obj.get("arguments");
+        Object[] arguments = new Object[method.getParameterTypes().length];
+        for (int i = 0; i < jsonArguments.size(); i++) {
+            arguments[i] = context.deserialize(jsonArguments.get(i), method
+                    .getGenericParameterTypes()[i]);
         }
 
-        MethodRecording methodRecording = new MethodRecording(clazz, method, arguments);
-		methodRecording.setReturnValue(returnValue);
+        MethodRecording methodRecording = new MethodRecording(clazz, method,
+                arguments);
+        methodRecording.setReturnValue(returnValue);
 
-		JsonArray jsonArray = (JsonArray) obj.get("dependencies");
+        JsonArray jsonArray = (JsonArray) obj.get("dependencies");
         for (JsonElement dependencyMethodRecording : jsonArray) {
-            methodRecording.addDependencyMethodCall(
-                    (MethodRecording) context.deserialize(dependencyMethodRecording, MethodRecording.class));
+            methodRecording.addDependencyMethodCall((MethodRecording) context
+                    .deserialize(dependencyMethodRecording,
+                            MethodRecording.class));
         }
 
-		return methodRecording;
-	}
+        return methodRecording;
+    }
 
     private Class<?> getTargetClass(JsonObject obj) {
         try {
-            return Class.forName(obj.getAsJsonPrimitive("classname").getAsString());
+            return Class.forName(obj.getAsJsonPrimitive("classname")
+                    .getAsString());
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/no/muda/jackbox/persistance/json/MethodTypeAdapter.java
+++ src/main/java/no/muda/jackbox/persistance/json/MethodTypeAdapter.java
@@ -15,64 +15,68 @@ import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
-public class MethodTypeAdapter implements JsonSerializer<Method>, JsonDeserializer<Method> {
-    public JsonElement serialize(Method src, Type typeOfSrc, JsonSerializationContext context) {
-	    String declaringClassName = src.getDeclaringClass().getCanonicalName();
-	    Class<?>[] parameters = src.getParameterTypes();
+public class MethodTypeAdapter implements JsonSerializer<Method>,
+        JsonDeserializer<Method> {
+    public JsonElement serialize(Method src, Type typeOfSrc,
+            JsonSerializationContext context) {
+        String declaringClassName = src.getDeclaringClass().getCanonicalName();
+        Class<?>[] parameters = src.getParameterTypes();
 
-	    JsonObject obj = new JsonObject();
-	    obj.addProperty("classname", declaringClassName);
-	    obj.addProperty("name", src.getName());
-	    obj.add("parameters", getJsonArrayForParameters(parameters));
+        JsonObject obj = new JsonObject();
+        obj.addProperty("classname", declaringClassName);
+        obj.addProperty("name", src.getName());
+        obj.add("parameters", getJsonArrayForParameters(parameters));
 
-	    return obj;
-	}
+        return obj;
+    }
 
-	private JsonArray getJsonArrayForParameters(Class<?>[] parameters) {
-		JsonArray parametersArray = new JsonArray();
-		for (Class<?> p : parameters) {
-			parametersArray.add(new JsonPrimitive(p.getCanonicalName()));
-		}
-		return parametersArray;
-	}
-
-	private Class<?>[] getParametersFromJsonArray(JsonArray array) throws ClassNotFoundException {
-		List<Class<?>> parameters = new LinkedList<Class<?>>();
-
-		for (JsonElement jsonElement : array) {
-		    String paramType = jsonElement.getAsString();
-		    if (paramType.equals("int")) {
-		        parameters.add(Integer.TYPE);
-		    } else {
-		        parameters.add(Class.forName(paramType));
-		    }
+    private JsonArray getJsonArrayForParameters(Class<?>[] parameters) {
+        JsonArray parametersArray = new JsonArray();
+        for (Class<?> p : parameters) {
+            parametersArray.add(new JsonPrimitive(p.getCanonicalName()));
         }
-		return parameters.toArray(new Class<?>[]{});
-	}
+        return parametersArray;
+    }
 
-	public Method deserialize(JsonElement element, Type typeOfSrc,
-				JsonDeserializationContext context) throws JsonParseException {
-			JsonObject obj = element.getAsJsonObject();
-			String className = obj.getAsJsonPrimitive("classname").getAsString();
-			Class<?>[] parameters;
-			try {
-				parameters = getParametersFromJsonArray(obj.get("parameters").getAsJsonArray());
-			} catch (ClassNotFoundException e) {
-				throw new JsonParseException(e);
-			}
+    private Class<?>[] getParametersFromJsonArray(JsonArray array)
+            throws ClassNotFoundException {
+        List<Class<?>> parameters = new LinkedList<Class<?>>();
 
-			String methodName = obj.getAsJsonPrimitive("name").getAsString();
+        for (JsonElement jsonElement : array) {
+            String paramType = jsonElement.getAsString();
+            if (paramType.equals("int")) {
+                parameters.add(Integer.TYPE);
+            } else {
+                parameters.add(Class.forName(paramType));
+            }
+        }
+        return parameters.toArray(new Class<?>[] {});
+    }
 
-			Method method;
-			try {
-				method = Class.forName(className).getMethod(methodName, parameters);
-			} catch (SecurityException e) {
-				throw new JsonParseException(e);
-			} catch (NoSuchMethodException e) {
-				throw new JsonParseException(e);
-			} catch (ClassNotFoundException e) {
-				throw new JsonParseException(e);
-			}
-			return method;
-		}
+    public Method deserialize(JsonElement element, Type typeOfSrc,
+            JsonDeserializationContext context) throws JsonParseException {
+        JsonObject obj = element.getAsJsonObject();
+        String className = obj.getAsJsonPrimitive("classname").getAsString();
+        Class<?>[] parameters;
+        try {
+            parameters = getParametersFromJsonArray(obj.get("parameters")
+                    .getAsJsonArray());
+        } catch (ClassNotFoundException e) {
+            throw new JsonParseException(e);
+        }
+
+        String methodName = obj.getAsJsonPrimitive("name").getAsString();
+
+        Method method;
+        try {
+            method = Class.forName(className).getMethod(methodName, parameters);
+        } catch (SecurityException e) {
+            throw new JsonParseException(e);
+        } catch (NoSuchMethodException e) {
+            throw new JsonParseException(e);
+        } catch (ClassNotFoundException e) {
+            throw new JsonParseException(e);
+        }
+        return method;
+    }
 }
